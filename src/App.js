8345ca1c653d1fb36ef6e671b28a251a4773fbe9
akine <weezer.and.you@gmail.com>
2020-11-20 23:09:53 +0900

    first init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Notes from "./components/Notes";
+import CreateNote from "./components/CreateNote";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/notes">
+        <Notes />
+      </Route>
+      <Route path="/create">
+        <CreateNote />
+      </Route>
+    </Router>
   );
 }
 
